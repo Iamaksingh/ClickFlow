2f7653ef72a8826c35309fd9e56d0d3ea2b0020c
Aditya Kumar Singh <iamaksingh2004@gmail.com>
2026-04-17 18:20:56 +0530

    standardized API responses using class objects in utils/apiResponse.js, implemented databases connection checking before starting server

--- backend/index.js
+++ backend/index.js
@@ -5,30 +5,42 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import connectDB from "./src/config/db.js";
+
 import healthCheckRoutes from "./src/routes/healthCheck.routes.js";
-// import linkRoutes from "./src/routes/link.routes.js";
-// import { redirectLink } from "./src/controllers/link.controller.js";
+
+import linkRoutes from "./src/routes/link.routes.js";
+import {redirectLink} from "./src/controllers/link.controller.js";
 
 //defining the port
 const PORT = process.env.PORT || 5000;
 
-
 //make an express app
 const app = express();
-
-//defining the database connection  {this is in the src/config/db.js file}
-connectDB();
 
 //defining the middleware
 app.use(cors());
 app.use(express.json());
 
-// app.use("/api/links", linkRoutes);
-// app.get("/:shortCode", redirectLink);
-
 //use health check routes
 app.use("/health", healthCheckRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+//use link routes to create short code
+app.use("/api/links", linkRoutes);
+
+//define route for redirecting to the original URL when a short URL is accessed
+app.get("/:shortCode", redirectLink);
+
+//before starting the server, connect to the database 
+const startServer = async () => { 
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect DB", error);
+    process.exit(1);
+  }
+};
+
+startServer();
