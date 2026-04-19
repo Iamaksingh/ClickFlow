872826de932bc0054fb6d33e16ba72ec86ea61e6
Aditya Kumar Singh <iamaksingh2004@gmail.com>
2026-04-20 01:42:36 +0530

    chore(core) : added cors configurations to support http cookies

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 //defining the middleware
-app.use(cors());
+app.use(cors({origin: "http://localhost:5173",credentials: true,}));
 app.use(express.json());
 app.use(cookieParser());
 
