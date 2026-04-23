52e287278c918d63502eac625aff0555866905d4
Aditya Kumar Singh <iamaksingh2004@gmail.com>
2026-04-23 06:31:29 +0530

    refactor(backend) : added frontend URL in enviornment variables

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 //defining the middleware
-app.use(cors({origin: "http://localhost:5173",credentials: true,}));
+app.use(cors({origin: process.env.FRONTEND_URL ,credentials: true,}));
 app.use(express.json());
 app.use(cookieParser());
 
