e97346e3d59ff1677b27b680367b2d788afb4e67
Aditya Kumar Singh <iamaksingh2004@gmail.com>
2026-04-23 06:41:08 +0530

    chore(backend) : added cross origin support in production setting

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,75 +3,76 @@ import ApiResponse from '../utils/apiResponse.js';
 import User from "../models/User.model.js";
 import validator from "validator";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 //signup controller 
 const signup = async (req, res) => {
     try {
         const { email, password } = req.body;
         if (!email || !password) {  // basic validation
-            return res.status(400).json( ApiResponse.error('Email and password are required') );
+            return res.status(400).json(ApiResponse.error('Email and password are required'));
         }
         if (!validator.isEmail(email, { require_tld: true })) {   //proper email check 
             return res.status(400).json(ApiResponse.error("Invalid email format"));
         }
         // check if user exists
         const normalizedEmail = email.toLowerCase().trim();
-        const existingUser = await User.findOne({ email : normalizedEmail });
+        const existingUser = await User.findOne({ email: normalizedEmail });
         if (existingUser) {
-            return res.status(409).json( ApiResponse.error('User already exists, login instead of signup') );
+            return res.status(409).json(ApiResponse.error('User already exists, login instead of signup'));
         }
         // create user (password gets hashed automatically)
-        const user = await User.create({ email : normalizedEmail, password });
+        const user = await User.create({ email: normalizedEmail, password });
         // generate token
         const token = generateToken(user._id);
 
         //using http cookies
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,    //  keep false for local dev
-            sameSite: "lax",
+            secure: isProduction,  // true on Railway
+            sameSite: isProduction ? "none" : "lax",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
-        res.status(201).json( ApiResponse.success('User created') ); 
+        res.status(201).json(ApiResponse.success('User created'));
     } catch (error) {
         console.error(error);
         res.status(500).json(ApiResponse.error('unable to signup', error.message));  //if unable to signup due to some reason 
     }
 };
 
-
 // login controller 
 const login = async (req, res) => {
     try {
         const { email, password } = req.body;
         if (!email || !password) {  // basic validation
-            return res.status(400).json( ApiResponse.error('Email and password are required') );
+            return res.status(400).json(ApiResponse.error('Email and password are required'));
         }
         if (!validator.isEmail(email, { require_tld: true })) {   //proper email check 
             return res.status(400).json(ApiResponse.error("Invalid email format"));
         }
         // check user exists
         const normalizedEmail = email.toLowerCase().trim();
-        const user = await User.findOne({ email : normalizedEmail }).select("+password");
+        const user = await User.findOne({ email: normalizedEmail }).select("+password");
         if (!user) {
             return res.status(401).json(ApiResponse.error('Invalid credentials'));
         }
         // compare password
         const isMatch = await user.comparePassword(password);
         if (!isMatch) {
-            return res.status(401).json( ApiResponse.error('Invalid credentials') );
+            return res.status(401).json(ApiResponse.error('Invalid credentials'));
         }
         // generate token
         const token = generateToken(user._id);
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,    //  keep false for local dev
-            sameSite: "lax",
+            secure: isProduction,              // true on Railway
+            sameSite: isProduction ? "none" : "lax",    //  keep false for local dev
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
-        res.status(200).json( ApiResponse.success('Login successful') );
+        res.status(200).json(ApiResponse.success('Login successful'));
     } catch (error) {
         console.error(error);
-        res.status(500).json( ApiResponse.error('Server error') );
+        res.status(500).json(ApiResponse.error('Server error'));
     }
 };
 
@@ -80,8 +81,8 @@ const logout = async (req, res) => {
     try {
         res.clearCookie("token", {
             httpOnly: true,
-            secure: false,
-            sameSite: "lax",
+            secure: isProduction,
+            sameSite: isProduction ? "none" : "lax",
         });
 
         return res.status(200).json(ApiResponse.success("Logout successful"));
@@ -92,11 +93,11 @@ const logout = async (req, res) => {
 };
 
 const generateToken = (userId) => {
-  return jwt.sign(
-    { userId },
-    process.env.JWT_SECRET,
-    { expiresIn: "7d" }
-  );
+    return jwt.sign(
+        { userId },
+        process.env.JWT_SECRET,
+        { expiresIn: "7d" }
+    );
 };
 
 export { signup, login, logout };
